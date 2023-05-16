52aae25c8308f556ba9827253188a110d4c01fbe
Atmik Tumane <atmiktumane72@gmail.com>
2023-05-16 19:28:16 +0530

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const { chats } = require("./data/data");
+const connectDB = require("./config/db");
+const colors = require("colors");
+const userRoutes = require("./routes/userRoutes");
+const chatRoutes = require("./routes/chatRoutes");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
+
+dotenv.config();
+
+connectDB();
+const app = express();
+
+app.use(express.json()); // to accept JSON Data
+
+app.get("/", (req, res) => {
+  res.send("API is Running Successfully");
+});
+
+app.use("/api/user", userRoutes);
+app.use("/api/chat", chatRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(5000, console.log(`Server Started on PORT ${PORT}`.yellow.bold));
